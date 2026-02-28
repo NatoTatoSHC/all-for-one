5ac2d5930d89702f736c22a1d0a1f082fd5adeb5
NatoTatoSHC <lesternate6@gmail.com>
2026-02-27 20:44:31 -0600

    commit

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const {createClient} = require("@supabase/supabase-js");
+let client = createClient("https://jgjdxlulszliepzrhgff.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnamR4bHVsc3psaWVwenJoZ2ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0ODc1ODIsImV4cCI6MjA4MTA2MzU4Mn0.OWdRl6uqpdui33Jg0g1SRMS3oeKVDUOBsogBovVnq_I");
+
+async function DB(sil, stat, miss) {
+    await client.from("silos").update({status: stat, missle: miss}).eq("silo", sil);
+}
 
 var app = express();
 var status = {"MO":{"D8":{"SG":{"J":[]}}}}
@@ -30,6 +36,11 @@ app.post("/status", (req, res) => {
     if (req.body.password == fs.readFileSync(path.resolve("password.txt"), "utf8")) {
         let silo = req.body.silo;
         let status = req.body.status;
+        let missle = req.body.missle;
+        DB(silo, status, missle);
+        res.send("Updated Status");
+    } else {
+        res.send("Incorrect Password");
     }
 })
 
