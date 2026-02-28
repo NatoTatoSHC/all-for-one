d4adaffa97f1b1d548e3a169feb4b42e6cd4c25e
NatoTatoSHC <lesternate6@gmail.com>
2026-02-27 23:14:58 -0600

    commit

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ async function DB(sil, stat, miss) {
 
 var app = express();
 var status = {"MO":{"D8":{"SG":{"J":[]}}}}
+var oneTimes = [];
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req,res) => {
@@ -18,30 +19,51 @@ app.get("/", (req,res) => {
 
 app.get("/app", (req, res) => {
     res.sendFile(path.resolve("./verify.html"));
-})
+});
+
+app.get("/status", (req, res) => {
+    res.sendFile(path.resolve("./status.html"));
+});
 
 app.post("/app", (req, res) => {
     if (req.body.loc == "") {
         res.redirect("/");
     }
-    if (req.body.password == fs.readFileSync(path.resolve("password.txt"), "utf8")) {
+    if (req.body.password == fs.readFileSync(path.resolve("password.txt"), "utf8") || oneTimes.includes(req.body.password)) {
         console.log("verified");
         res.sendFile(path.resolve(req.body.loc+".html"));
+        if (oneTimes.includes(req.body.password)) {
+            oneTimes.splice(oneTimes.indexOf(req.body.password));
+        }
     } else {
         console.log("deniaed");
     }
 })
 
 app.post("/status", (req, res) => {
-    if (req.body.password == fs.readFileSync(path.resolve("password.txt"), "utf8")) {
-        let silo = req.body.silo;
-        let status = req.body.status;
-        let missle = req.body.missle;
-        DB(silo, status, missle);
-        res.send("Updated Status");
+    console.log("Status Update Request");
+    console.log(req.body);
+    if (req.body.oneTime) {
+        if (req.body.password == fs.readFileSync(path.resolve("password.txt"), "utf8")) {
+            if (!oneTimes.includes(req.body.oneTime)) {
+                oneTimes.push(req.body.oneTime);
+            }
+            res.redirect("/status");
+        } else {
+            res.send("Incorrect Password");
+        }
     } else {
-        res.send("Incorrect Password");
+        if (req.body.password == fs.readFileSync(path.resolve("password.txt"), "utf8")) {
+            let silo = req.body.silo;
+            let status = req.body.status;
+            let missle = req.body.missle;
+            DB(silo, status, missle);
+            res.send("Updated Status");
+        } else {
+            res.send("Incorrect Password");
+        }
     }
-})
+    console.log(oneTimes);
+});
 
 app.listen(3000);
